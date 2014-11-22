c3544d171691512043f6bc01125ca0e72f01099e
Adel Lahlou <adellahlou@users.noreply.github.com>
2014-11-22 17:11:32 -0600

    Updated user_id (int) to username (varchar)

--- switch_declare.sql
+++ switch_declare.sql
@@ -9,20 +9,20 @@ CREATE TABLE `groups` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `users` (
-  `user_id` int(10) NOT NULL,
+  `username` varchar(45) NOT NULL,
   `password` varchar(45) NOT NULL,
-  PRIMARY KEY (`user_id`)
+  PRIMARY KEY (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `in_group` (
-  `user_id` int(11) NOT NULL,
+  `username` varchar(45) NOT NULL,
   `in_id` int(11) NOT NULL,
   `group_score` int(11) NOT NULL DEFAULT '0',
-  `group.id` int(11) DEFAULT NULL,
+  `group_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`in_id`),
-  KEY `fk_user_id_idx` (`user_id`),
-  KEY `fk_group_id_idx` (`group.id`),
-  CONSTRAINT `fk_group_id` FOREIGN KEY (`group.id`) REFERENCES `groups` (`group_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  KEY `fk_username_idx` (`username`),
+  KEY `fk_group_id_idx` (`group_id`),
+  CONSTRAINT `fk_group_id` FOREIGN KEY (`group_id`) REFERENCES `groups` (`group_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_username` FOREIGN KEY (`username`) REFERENCES `users` (`username`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
