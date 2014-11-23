0bac69a3919211ea4c0a48a1d19b9eed97789376
Adel Lahlou <adellahlou@users.noreply.github.com>
2014-11-22 21:38:12 -0600

    Fixed the last of the SQL

--- switch-populate.sql
+++ switch-populate.sql
@@ -34,39 +34,40 @@ VALUES ('Go Catz');
 
 --join users to groups
 
-INSERT INTO `switch`.`in_group` ('in_group'.`username`,`in_group`.`group_id`)
+INSERT INTO `switch`.`in_group` (`in_group`.`username`,`in_group`.`group_id`)
 VALUES ('tester6', 1);
 
-INSERT INTO `switch`.`in_group` ('in_group'.`username`,`in_group`.`group_id`)
+INSERT INTO `switch`.`in_group` (`in_group`.`username`,`in_group`.`group_id`)
 VALUES ('tester1', 1);
 
 
 
-INSERT INTO `switch`.`in_group` ('in_group'.`username`,`in_group`.`group_id`)
+INSERT INTO `switch`.`in_group` (`in_group`.`username`,`in_group`.`group_id`)
 VALUES ('tester6', 0);
 
-INSERT INTO `switch`.`in_group` ('in_group'.`username`,`in_group`.`group_id`)
+INSERT INTO `switch`.`in_group` (`in_group`.`username`,`in_group`.`group_id`)
 VALUES ('tester1', 0);
 
 
 
-INSERT INTO `switch`.`in_group` ('in_group'.`username`,`in_group`.`group_id`)
+INSERT INTO `switch`.`in_group` (`in_group`.`username`,`in_group`.`group_id`)
 VALUES ('tester6', 0);
 
-INSERT INTO `switch`.`in_group` ('in_group'.`username`,`in_group`.`group_id`)
+INSERT INTO `switch`.`in_group` (`in_group`.`username`,`in_group`.`group_id`)
 VALUES ('tester5', 2);
 
-INSERT INTO `switch`.`in_group` ('in_group'.`username`,`in_group`.`group_id`)
+INSERT INTO `switch`.`in_group` (`in_group`.`username`,`in_group`.`group_id`)
 VALUES ('tester4', 2);
 
-INSERT INTO `switch`.`in_group` ('in_group'.`username`,`in_group`.`group_id`)
+INSERT INTO `switch`.`in_group` (`in_group`.`username`,`in_group`.`group_id`)
 VALUES ('tester3', 2);
 
-INSERT INTO `switch`.`in_group` ('in_group'.`username`,`in_group`.`group_id`)
+INSERT INTO `switch`.`in_group` (`in_group`.`username`,`in_group`.`group_id`)
 VALUES ('tester2', 2);
 
-INSERT INTO `switch`.`in_group` ('in_group'.`username`,`in_group`.`group_id`)
+INSERT INTO `switch`.`in_group` (`in_group`.`username`,`in_group`.`group_id`)
 VALUES ('tester6', 2);
 
+exit;
 
 
--- switch_declare.sql
+++ switch_declare.sql
@@ -1,34 +1,35 @@
 CREATE DATABASE switch;
+
 USE switch;
 
 
-CREATE TABLE `groups` (
-  `group_id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) NOT NULL,
-  `data` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`group_id`)
+CREATE TABLE groups (
+  group_id int(11) NOT NULL AUTO_INCREMENT,
+  name varchar(45) NOT NULL,
+  data varchar(255) DEFAULT NULL,
+  PRIMARY KEY (group_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-ALTER TABLE `switch`.`groups` AUTO_INCREMENT = 0; 
+ALTER TABLE groups AUTO_INCREMENT = 0; 
 
-CREATE TABLE `users` (
-  `username` varchar(45) NOT NULL,
-  `password` varchar(45) NOT NULL,
-  PRIMARY KEY (`username`)
+CREATE TABLE users (
+  username varchar(45) NOT NULL,
+  password varchar(45) NOT NULL,
+  PRIMARY KEY (username)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE `in_group` (
-  `in_id` int(11) NOT NULL AUTO_INCREMENT,
-  `group_id` int(11) NOT NULL,
-  `username` varchar(45) NOT NULL,
-  `group_score` INT NOT NULL DEFAULT 0,
-  PRIMARY KEY (`in_id`),
-  KEY `fk_username_idx` (`username`),
-  KEY `fk_group_id_idx` (`group_id`),
-  CONSTRAINT `fk_group_id` FOREIGN KEY (`group_id`) REFERENCES `groups` (`group_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_username` FOREIGN KEY (`username`) REFERENCES `users` (`username`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `one-to-one` UNIQUE  (`username`,`group_id`)
+CREATE TABLE in_group (
+  in_id int(11) NOT NULL AUTO_INCREMENT,
+  group_id int(11) NOT NULL,
+  username varchar(45) NOT NULL,
+  group_score INT NOT NULL DEFAULT 0,
+  PRIMARY KEY (in_id),
+  KEY fk_username_idx (username),
+  KEY fk_group_id_idx (group_id),
+  CONSTRAINT fk_group_id FOREIGN KEY (group_id) REFERENCES groups (group_id) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT fk_username FOREIGN KEY (username) REFERENCES users (username) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  UNIQUE  (username, group_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-ALTER TABLE `switch`.`in_group` AUTO_INCREMENT = 0;
+ALTER TABLE in_group AUTO_INCREMENT = 0;
 
